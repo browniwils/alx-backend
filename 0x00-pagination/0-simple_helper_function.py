539a3a915dd54e7016731337c480b96c4ee822bc
browniwils <browniwils@gmail.com>
2024-01-10 12:40:15 +0000

    completed project 0x00

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Module for a simple pagination.
+"""
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Creates paginationg with start and size of page.
+    """
+    if isinstance(page, int) and isinstance(page_size, int):
+        return ((page - 1) * page_size, page_size * page)
+    raise TypeError('Expected `page` and `page_size` to be ints')
